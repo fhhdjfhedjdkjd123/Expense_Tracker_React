71023137bd72d2ae353ae0973e016f32ca9c3604
madhuri <madhuriaileni2320@gmail.com>
2023-07-12 13:32:36 +0530

    signup-screen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React from 'react';
+import { Switch, Route  } from 'react-router-dom';
+import Auth from './Authentication/Auth';
+const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Switch>
+        <Route path="/" exact> */}
+          <Auth/>
+        {/* </Route>
+      </Switch> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './Components/store/AuthContext';
+
+//React Bootstrap Configurarion
+import "../node_modules/react-bootstrap/dist/react-bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+root.render
+(
+    <AuthProvider>
+        <App />
+    </AuthProvider>
+    
 );
 
 // If you want to start measuring performance in your app, pass a function
